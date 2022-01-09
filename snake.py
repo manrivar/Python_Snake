9f1f6625c4e72ee73d6c6433272d7dfe5d73457d
manrivar <manrivargueta@gmail.com>
2022-01-08 23:34:29 -0600

    Proyecto_finalizado

---
+++ snake.py
@@ -0,0 +1,153 @@
+import turtle
+import time
+import random
+
+posponer = 0.1
+
+# Marcador
+score = 0
+highscore = 0
+
+# Configuracion de la ventana
+wn = turtle.Screen()
+wn.title("Juego de Snake")
+wn.bgcolor("black")
+wn.setup(width = 600, height = 600)
+wn.tracer(0)
+
+# Cabeza de la serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "stop"
+
+# Comida
+comida = turtle.Turtle()
+comida.speed(0)
+comida.shape("circle")
+comida.color("red")
+comida.penup()
+comida.goto(0,100)
+
+# Cuerpo serpiente l
+segmentos =[]
+
+# Texto 
+texto = turtle.Turtle()
+texto.speed(0)
+texto.color("white")
+texto.penup()
+texto.hideturtle()
+texto.goto(0,260)
+texto.write("Score: 0               High Score: 0", align = "center", font = ("Courier", 15, "normal"))
+
+# Funciones
+def arriba():
+    cabeza.direction = "up"
+def abajo():
+    cabeza.direction = "down"
+def izquierda():
+    cabeza.direction = "left"
+def derecha():
+    cabeza.direction = "right"
+
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+    
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
+    
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
+
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
+
+#Teclado
+wn.listen()
+wn.onkeypress(arriba, "Up")
+wn.onkeypress(abajo, "Down")
+wn.onkeypress(izquierda, "Left")
+wn.onkeypress(derecha, "Right")
+
+while True:
+    wn.update()
+
+    # Colision con bordes
+    if cabeza.xcor() > 280 or cabeza.xcor() < -280 or cabeza.ycor() > 280 or cabeza.ycor() < -280:
+        time.sleep(1)
+        cabeza.goto(0,0)
+        cabeza.direction = "stop"
+
+        #esconder los segmentos (investigar si se pueden borrar con el modulo turtle)
+        # for segmento in segmentos:
+        #     segmento.goto(1000,1000)
+        [i.hideturtle() for i in segmentos]
+        segmentos.clear()
+
+        # Reset el marcador del
+        score = 0
+        texto.clear()
+        texto.write("Score: {}          High Score: {}".format(score, highscore), align = "center", font = ("Courier", 15, "normal"))
+
+    # Colision con la comida
+    if cabeza.distance(comida) < 20:
+        x = random.randint(-280,280)
+        y = random.randint(-280,220)
+        comida.goto(x,y)
+
+        nuevo_segmento = turtle.Turtle()
+        nuevo_segmento.speed(0)
+        nuevo_segmento.shape("square")
+        nuevo_segmento.color("grey")
+        nuevo_segmento.penup()
+        segmentos.append(nuevo_segmento)
+
+        # Aumentar marcador
+        score += 10
+
+        if score > highscore:
+            highscore = score
+        
+        texto.clear()
+        texto.write("Score: {}          High Score: {}".format(score, highscore), align = "center", font = ("Courier", 15, "normal"))
+
+    # Mover el cuerpo de la serpiente
+    totalSeg = len(segmentos)
+    for index in range(totalSeg -1, 0, -1):
+        x = segmentos[index - 1].xcor()
+        y = segmentos[index - 1].ycor()
+        segmentos[index].goto(x,y)
+    
+    if totalSeg > 0:
+        x = cabeza.xcor()
+        y = cabeza.ycor()
+        segmentos[0].goto(x,y)
+
+    mov()
+
+    # Colision con el cuerpo 
+    for segmento in segmentos:
+        if segmento.distance(cabeza) < 20:
+            time.sleep(1)
+            cabeza.goto(0,0)
+            cabeza.direction = "stop"
+
+            # Eliminar segmentos
+            [i.hideturtle() for i in segmentos]
+            segmentos.clear()
+
+            # Reset el marcador del
+            score = 0
+            texto.clear()
+            texto.write("Score: {}          High Score: {}".format(score, highscore), align = "center", font = ("Courier", 15, "normal"))
+
+    time.sleep(posponer)
